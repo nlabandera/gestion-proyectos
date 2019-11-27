f4e30e0e3a1881b9288d9b258303c05a1fa41052
“ajuanenama” <ajuanenama@zubirimanteo.com>
2019-11-27 10:59:59 +0100

    7 metodos empleado

--- app/Http/Controllers/EmpleadoController.php
+++ app/Http/Controllers/EmpleadoController.php
@@ -19,7 +19,52 @@ class EmpleadoController extends Controller
 
     	$empleadoInfo = Empleado::find($id);
         return view('empleados.empleado', ['empleadoInfo'=>$empleadoInfo]);
+        //return view('empleados.empleado', compact('empleadoInfo')); -->Cuando clave y valor se llaman igual
 
+    }
+    public function create()
+    {
+        return view ('empleados.create');
+    }
+     public function store(Request $request)
+    {
+        $empleado = new Empleado();
+
+        $empleado->nombre = $recuest->nombre;
+        $empleado->apellido = $recuest->apellido;
+        $empleado->email = $recuest->email;
+        $empleado->telefono = $recuest->telefono;
+        $empleado->safe();
+
+    return redirect(/*route(empleados.index*/);     //Dirigir a la página que se quiera para mostrar datos
+    }
+    public function edit($id)
+    {
+        $empleadoInfo = Empleado::find($id);
+        return view('empleados.edit', ['empleadoInfo'=>$empleadoInfo]);
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, $id)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy($id)
+    {
+        //
     }
 
 
--- routes/web.php
+++ routes/web.php
@@ -17,10 +17,20 @@ Route::get('/', function () {
 // Empleados
 Route::get('empleados','EmpleadoController@index')->name('empleados.index');
 Route::get('empleados/{id}','EmpleadoController@show')->name('empleados.empleado');
+Route::get('empleados/create','EmpleadoController@create')->name('empleados.create');
+Route::get('empleados/{id}/edit','EmpleadoController@edit')->name('empleados.edit');
+Route::put('empleados/{id}','EmpleadoController@update')->name('empleados.update');
+Route::delete('empleados/{id}','EmpleadoController@destroy')->name('empleados.destroy');
+Route::post('empleados','EmpleadoController@store')->name('empleados.store');
 
 // Proyectos
 Route::get('proyectos','ProyectoController@index')->name('proyectos.index');
 Route::get('proyectos/{id}','ProyectoController@show')->name('proyectos.proyecto');
+Route::get('proyectos/create','ProyectoController@create')->name('create.proyeto');
+Route::get('proyectos/{id}/edit','ProyectoController@edit')->name('edit.proyeto');
+Route::put('proyectos/{id}','ProyectoController@update')->name('update.proyeto');
+Route::delete('proyectos/{id}','ProyectoController@destroy')->name('destroy.proyeto');
+Route::post('proyectos','ProyectoController@store')->name('store.proyeto');
 //Departamentos
 Route::get('departamentos','DepartamentoController@index')->name('departamentos.index');
 Route::get('departamentos/{id}','DepartamentoController@show')->name('departamentos.departamento');
